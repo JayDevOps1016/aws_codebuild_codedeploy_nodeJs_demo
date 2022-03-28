72a5fdfb4756308fcf39588e17dfc051a90da198
jaydeepvaghela_crest <jaydeep.vaghela@crestdatasys.com>
2022-03-28 15:50:06 +0530

    this commit for the testing of github web hook

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get('/status', (req, res) => {
 
 
 app.listen(port, () => {
-  	console.log(`this time build start by githubWebHook- jay`)
+  	console.log(`this time build start by githubWebHook and type of pipeline project- jay`)
 	console.log(`Example app listening at http://localhost:${port}`)
 })
